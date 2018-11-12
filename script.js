129f1aceda3be5b4aec6855318032edc459e45c9
Laura <laura.jinks@gmail.com>
2018-11-12 13:06:15 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Add button clicked
+// If text entered in input field, add text to list
+document.getElementById('addItem').addEventListener('click', function() {
+    let value = document.getElementById('newItem')
+});
